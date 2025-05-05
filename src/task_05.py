dbf393dcc5960bfd681a0efdd5d2e876617a170e
Elektr0ni3561 <shokoladka3561@gmail.com>
2025-05-06 00:14:03 +0300

    Task 5

---
+++ src/task_05.py
@@ -0,0 +1,18 @@
+# Разработать метод date_in_future(integer), который вернет дату через integer дней.
+# Если integer не является целым числом, то метод должен вывести текущую дату.
+# Формат возвращаемой методом даты должен иметь следующий вид '24-03-2001
+# 22:33:44'.
+
+from datetime import datetime, timedelta
+
+DATE_PATTERN = "%d-%m-%Y %H:%M:%S"
+
+def date_in_future(integer):
+    if not isinstance(integer, int):
+        print(datetime.today().strftime(DATE_PATTERN))
+        return
+    print((datetime.today() + timedelta(days=integer)).strftime(DATE_PATTERN))
+
+if __name__ == "__main__":
+    date_in_future([]) # => текущая дата
+    date_in_future(2) # => текущая дата + 2 дня
