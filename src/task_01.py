22e18201ed499acc455046e1123d7883d1181f50
Elektr0ni3561 <shokoladka3561@gmail.com>
2025-05-05 21:11:00 +0300

    Task 1

---
+++ src/task_01.py
@@ -0,0 +1,17 @@
+import re
+
+def is_palindrome(text):
+    if type(text) is None: return False
+    sub_string = re.sub(r"[^a-zA-Zа-яА-Я\d]", "", text.__str__())
+    sub_string = sub_string.lower()
+    for i in range(len(sub_string) // 2):
+        if sub_string[i] != sub_string[-i-1]: return False
+    return True
+
+
+if __name__ == "__main__":
+    print(is_palindrome("A man, a plan, a canal -- Panama"))  # => True
+    print(is_palindrome("Madam, I'm Adam!")) # => True
+    print(is_palindrome(333))  # => True
+    print(is_palindrome(None))  # => False
+    print(is_palindrome("Abracadabra"))  # => False
