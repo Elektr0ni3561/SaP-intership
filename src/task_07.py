8ad9a3b4b2ebbdf2093e0eaea0129e7be1a325d4
Elektr0ni3561 <shokoladka3561@gmail.com>
2025-07-07 17:22:41 +0300

    Task 7

---
+++ src/task_07.py
@@ -0,0 +1,22 @@
+# Анаграмма — литературный приём, состоящий в перестановке букв или звуков
+# определённого слова (или словосочетания), что в результате даёт другое слово
+# или словосочетание.
+# Разработайте метод combine_anagrams(words_array), который принимает на вход
+# массив слов и разбивает их в группы по анаграммам, регистр букв не имеет
+# значения при определении анаграмм.
+
+def combine_anagrams(words_array: list):
+    map = {}
+    for word in words_array:
+        key="".join(sorted(word.lower()))
+        if not map.get(key):
+            map[key]=[]
+        map.get(key).append(word.lower())
+    anagrams = []
+    for array in map.values():
+        anagrams.append(array)
+    return anagrams
+
+if __name__ == "__main__":
+    print(combine_anagrams(["Cars", "For", "Potatoes", "Racs", "Four", "Scar", "Creams", "Scream"]))
+    # => [['cars', 'racs', 'scar'], ['for'], ['potatoes'], ['four'], ['creams', 'scream']]
